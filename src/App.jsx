f41729c4be40255e67f9342c808b0a6e62f3646a
Maria Munoz Rodriguez <1593maria@gmail.com>
2022-10-06 11:55:25 +0200

    fonts and path

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import LogoSneakers from './assets/images/logo.svg'
+
+
+const App = () => {
+    return (<>
+        <header>
+        <img src={LogoSneakers} alt="" />
+        </header>
+    </>)
+}
+
+export default App
