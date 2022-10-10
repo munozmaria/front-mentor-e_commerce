c36bea8edeef2fbb3c61c07d4abb7a05338854d1
Maria Munoz Rodriguez <1593maria@gmail.com>
2022-10-10 17:15:10 +0200

    button and nav mediaqueries

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -1,10 +1,16 @@
 import LogoSneakers from "@/assets/images/logo.svg"
 import Avatar from "@/assets/images/image-avatar.png"
+import MenuIcon from "@/components/icons/MenuIcon"
+
+
 const MainHeader = () => {
 	return (
-		<header>
+        <header>
+            <button className="md:hidden">
+                <MenuIcon></MenuIcon>
+            </button>
 			<img src={LogoSneakers} alt="" />
-			<nav>
+			<nav className="hidden md:block">
 				<a href="#">Collections</a>
 				<a href="#">Men</a>
 				<a href="#">Women</a>
